b5e33e8c23ea04b397ed5a5ea06796dcc324925d
joseph.yang <before30@gmail.com>
2020-11-23 00:19:52 +0900

    Polish

--- r4j-api/src/main/java/me/home/r4jex/api/domain/exception/BizException.java
+++
@@ -1,7 +0,0 @@
-package me.home.r4jex.api.domain.exception;
-
-public class BizException extends RuntimeException {
-    public BizException(String msg) {
-        super(msg);
-    }
-}
--- r4j-api/src/main/java/me/home/r4jex/api/domain/exception/NonBizException.java
+++
@@ -1,7 +0,0 @@
-package me.home.r4jex.api.domain.exception;
-
-public class NonBizException extends RuntimeException {
-    public NonBizException(String msg) {
-        super(msg);
-    }
-}
